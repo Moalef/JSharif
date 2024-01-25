85524f86b98c1ef48553ebd0e534ed75f9defafe
Mojo <moalef@live.com>
2024-01-25 19:00:04 +0330

    added a line counter and a sentence counter

--- Midterm1.py
+++ Midterm1.py
@@ -1,14 +1,28 @@
 class TextAnalyzer:
     def __init__(self, input_path, output_path, ignored_path, min_word_len = 0, 
                  max_word_len = None , consec_words = 1, sorted = None):
-        self.input_path = input_path
-        self.output_path = output_path
-        self.ignored_path = ignored_path
-        self.min_word_len = min_word_len
-        self.max_word_len = max_word_len
-        self.consec_words = consec_words
-        self.sorted = sorted
+        self._input_path = input_path
+        self._output_path = output_path
+        self._ignored_path = ignored_path
+        self._min_word_len = min_word_len
+        self._max_word_len = max_word_len
+        self._consec_words = consec_words
+        self._sorted = sorted
     
-    def analyze(self):
-        with open(self.input_path, 'r') as input_file:
-            input_file.read()
+    def _line_count(self):
+        with open(self._input_path, 'r') as input_file:
+            return len(input_file.readlines())
+        
+    def _sentence_count(self):
+        num_sentence = 0
+        with open(self._input_path, 'r') as input_file:
+            for line in input_file:
+                num_sentence += line.count('.')
+                num_sentence += line.count('!')
+                num_sentence += line.count('?')
+        return num_sentence
+
+
+
+test = TextAnalyzer(r'C:\Mojo\Prog\Python\JSharif\Exercise 3\test_input.txt' , 'C:\\' , 'D:\\')
+
